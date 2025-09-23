12d5c0d2efa8ff917e96a42ab096dff4ce50b9fa
Victor Grinchik <victor.grinchik@gmail.com>
2025-09-23 19:34:26 +0300

    Implementing subtree_root_list function

---
+++ duplicated_subtrees/subtree_root_list.py
@@ -0,0 +1,25 @@
+import itertools
+import os
+
+from duplicated_subtrees_types import DuplicatedSubTreeList
+
+def subtree_root_list(
+    subtree_list: DuplicatedSubTreeList, 
+) -> DuplicatedSubTreeList:
+    dir_set = set(itertools.chain.from_iterable(subtree_list))
+
+    result: DuplicatedSubTreeList = []
+
+    for dup_dir_path_list in subtree_list:
+        subresult = \
+            list(
+                filter(
+                    lambda dir_path:
+                        not os.path.dirname(dir_path) in dir_set,
+                        dup_dir_path_list
+                    ),
+                )
+
+        result.append(subresult)
+
+    return list(filter(lambda x: len(x) > 1, result))
